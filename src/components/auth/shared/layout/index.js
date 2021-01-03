f80df630163c6b40188950e98e7128b3286c9684
Alejandro Tovar <javi.monsi2009@hotmail.com>
2021-01-02 19:27:06 -0600

    Template de registro e inicio terminada.

--- src/components/auth/shared/layout/index.js
+++ src/components/auth/shared/layout/index.js
@@ -1,5 +1,8 @@
 import styles from "./styles.module.scss"
 
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import EmailOutlinedIcon from '@material-ui/icons/EmailOutlined';
+
 // Next Components
 import Link from 'next/link'
 
@@ -10,33 +13,34 @@ export default function Layout({children}) {
             <div>
                 <img src="/tweeter-light.svg" />
 
-                <h3>Iniciar sesión</h3>
+                <p>Iniciar sesión</p>
 
-                <form>
-                    <label>
-                        <input type="text"></input>
+                <form className={styles.form} method="POST" action="">
+
+                    <label for="name" className={styles.label}>
+                        <EmailOutlinedIcon/>
+                        <input type="text" id="name" className={styles.input} placeholder="Correo"></input>
                     </label>
-                    <label>
-                        <input type="text"></input>
+
+                    <label for="passowrd" className={styles.label}>
+                        <LockOutlinedIcon/>
+                        <input type="password" id="password" className={styles.input} placeholder="Contraseña"></input>
                     </label>
 
                     <label>
-                        <input type="submit"></input>
+                        <input type="submit" id="submit" className={styles.submit} value="Iniciar sesión"></input>
                     </label>
                 </form>
 
                 <p>O continua con una de la siguientes redes sociales</p>
 
-                <div>
-                    
+                <div className={styles.social}>
                     <img src="Google.svg" />
                     <img src="Facebook.svg" />
                     <img src="Twitter.svg" />
                     <img src="Github.svg" />
                 </div>
-                <p>¿No tienes cuenta?  <Link href="/login">
-                            <a >Regístrate</a>
-                        </Link> </p>
+                <p>¿No tienes cuenta?  <Link href="/login"><a className={styles.link} >Regístrate</a></Link> </p>
             </div>
         </main>
     )
