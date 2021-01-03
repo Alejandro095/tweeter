1cc9d3dc7a5684fef910e40af409ce5750a531d6
Alejandro Tovar <javi.monsi2009@hotmail.com>
2021-01-02 21:32:20 -0600

    Se ha agregado modificado la letra Nono Sans y se agregado la letra Poppins.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
             <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&family=Poppins:wght@600&display=swap" rel="stylesheet" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
